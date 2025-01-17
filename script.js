ce3ec8c1363fef4f0d6896dbdf48901a821c780a
doublemovefb <doublemovebiz@gmail.com>
2025-01-16 20:51:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("NFL Draft Database loaded!");
+fetch('prospects.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(prospect => {
+            console.log(`${prospect.name} - ${prospect.position}`);
+        });
+    })
+    .catch(error => console.error("Error loading prospects:", error));
